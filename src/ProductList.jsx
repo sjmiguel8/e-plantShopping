df206908d63ba11ccd95c194073b313a0ea81749
SJMiguel8 <bonilla.miguel.e@gmail.com>
2024-12-17 18:52:33 -0600

    Updated im ages to show on ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,16 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
+import addItem from './CartSlice.jsx'
+
 function ProductList() {
-  
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+         }));
+      };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -251,11 +260,26 @@ function ProductList() {
         </div>
 
         <div className="product-grid">
-
+        {plantsArray.map((category, index) => (
+   
+        <div key={index}>
+        <h1><div>{category.category}</div></h1>
+        <div className="product-list">
+            {category.plants.map((plant, plantIndex) => (
+            <div className="product-card" key={plantIndex}>
+                <img className="product-image" src={plant.image} alt={plant.name} />
+                <div className="product-title">{plant.name}</div>
+                {/*Similarly like the above plant.name show other details like description and cost*/}
+                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+            </div>
+            ))}
+            </div>
+        </div>
+        ))}
 
         </div>
 
-    </div>
+        </div>
     );
 }
 
